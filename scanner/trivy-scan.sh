1ce797f90c27e1ef9fdef7a8351fe475185aac58
Srilekhasrii <110712403+Srilekhasrii@users.noreply.github.com>
2025-12-12 10:09:44 +0530

    Update trivy-scan.sh

--- scanner/trivy-scan.sh
+++ scanner/trivy-scan.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+IMAGE="$1"
+
+echo "🔍 Scanning image: $IMAGE"
+
+trivy image --severity HIGH,CRITICAL --format json -o scan-report.json $IMAGE
+
+if [ $? -ne 0 ]; then
+  echo "❌ Trivy scan failed"
+  exit 1
+fi
+
+CRITICAL=$(jq '.Results[].Vulnerabilities[] | select(.Severity=="CRITICAL") | .Severity' scan-report.json | wc -l)
+HIGH=$(jq '.Results[].Vulnerabilities[] | select(.Severity=="HIGH") | .Severity' scan-report.json | wc -l)
+
+echo "Critical: $CRITICAL | High: $HIGH"
+
+if [ "$CRITICAL" -gt 0 ] || [ "$HIGH" -gt 0 ]; then
+  echo "❗ Vulnerabilities found. Needs rebuild."
+  exit 2
+else
+  echo "✔ Image is secure."
+fi
 
