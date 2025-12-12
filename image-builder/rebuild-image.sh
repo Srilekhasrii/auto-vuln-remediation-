1050aa403b19c61a144b8db90e4b831223763a5b
Srilekhasrii <110712403+Srilekhasrii@users.noreply.github.com>
2025-12-12 10:14:34 +0530

    Update rebuild-image.sh

--- image-builder/rebuild-image.sh
+++ image-builder/rebuild-image.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+BASE_IMAGE="$1"
+
+DATE=$(date +%F-%H%M)
+NEW_TAG="secure-$DATE"
+
+echo "🔧 Rebuilding image: $BASE_IMAGE → $NEW_TAG"
+
+cp Dockerfile.template Dockerfile
+
+docker build -t $BASE_IMAGE:$NEW_TAG .
+
+echo "📤 Pushing image to registry..."
+docker push $BASE_IMAGE:$NEW_TAG
+
+echo $BASE_IMAGE:$NEW_TAG > new-image.txt
+
+echo "✔ Rebuild complete"
 
