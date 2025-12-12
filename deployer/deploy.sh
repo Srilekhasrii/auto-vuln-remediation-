0c9f2132f38aed2d8bdeafd34a3eb452b40d919b
Srilekhasrii <110712403+Srilekhasrii@users.noreply.github.com>
2025-12-12 10:18:49 +0530

    Update deploy.sh

--- deployer/deploy.sh
+++ deployer/deploy.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+NEW_IMAGE=$(cat ../image-builder/new-image.txt)
+
+echo "🚀 Deploying secured image: $NEW_IMAGE"
+
+sed -i "s|IMAGE_PLACEHOLDER|$NEW_IMAGE|g" k8s/deployment.yaml
+
+kubectl apply -f k8s/deployment.yaml
+kubectl rollout restart deployment/myapp
+
+echo "✔ Deployment done"
 
